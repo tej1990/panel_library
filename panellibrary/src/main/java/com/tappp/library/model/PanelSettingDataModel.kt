02dd1a146429e7dca1b836320e458204f096e9c8
Tejas A. Prajaapti <tejas.prajapati@zibtek.com>
2022-12-21 20:05:27 +0530

    - add methods

---
+++ panellibrary/src/main/java/com/tappp/library/model/PanelSettingDataModel.kt
@@ -0,0 +1,6 @@
+package com.tappp.library.model
+
+class PanelSettingDataModel {
+    var supportManager: Any? = null
+    var containerView: Any? = null
+}
--- panellibrary/src/main/java/com/tappp/library/view/OverlayPanelLayout.kt
+++ panellibrary/src/main/java/com/tappp/library/view/OverlayPanelLayout.kt
@@ -5,11 +5,14 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.panel.library.R
 import com.tappp.library.constant.Constants
+import com.tappp.library.model.PanelSettingDataModel
 import com.tappp.library.view.fragment.ImageFragment
 import com.tappp.library.view.fragment.WebFragment
+import org.json.JSONObject
 
 /**
  * Created by Tejas A. Prajapati on 30/11/22.
@@ -17,12 +20,16 @@ import com.tappp.library.view.fragment.WebFragment
 class OverlayPanelLayout : FrameLayout {
 
     private val TAG_FLUTTER_FRAGMENT = "flutter_fragment"
+
     /*private var flutterFragment: FlutterFragment? = null
     private val TAG_FLUTTER_FRAGMENT = "flutter_fragment"
-    private var supportFragmentManager: FragmentManager? = null
     private var fragmentContainer: Int? = null
     private var flutterEngine: FlutterEngine? = null
     private var channel: MethodChannel? = null*/
+    private var mFragment: Fragment = ImageFragment()
+    private var supportFragmentManager: FragmentManager? = null
+    private var tapppViewContainer: Int = 0
+    private lateinit var panelData: JSONObject
 
     constructor(context: Context) : super(context) {
     }
@@ -37,120 +44,86 @@ class OverlayPanelLayout : FrameLayout {
     ) {
     }
 
-    fun init(imgView: AppCompatImageView){
-        imgView.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.panel_pic))
+    fun init(mViewEnum: String, supportFragmentManager: FragmentManager, tapppViewContainer: Int) {
+        this.supportFragmentManager = supportFragmentManager
+        this.tapppViewContainer = tapppViewContainer
+        if (mViewEnum.equals(Constants.LOCAL_WEB_VIEW)) {
+            mFragment = WebFragment.newInstance(1)
+        } else if (mViewEnum.equals(Constants.S3_WEB_VIEW)) {
+            mFragment = WebFragment.newInstance(2)
+        } else if (mViewEnum.equals(Constants.LIBRARY_CALENDER)) {
+            mFragment = WebFragment.newInstance(3)
+        } else if (mViewEnum.equals(Constants.LIBRARY_PANEL)) {
+            mFragment = WebFragment.newInstance(5)
+        }
+        start()
+        /*this.supportFragmentManager = supportFragmentManager
+            this.fragmentContainer = fragmentContainer;
+            flutterFragment = supportFragmentManager
+                .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
+            if (flutterFragment == null) {
+                flutterEngine = FlutterEngine(context)
+                flutterEngine!!.dartExecutor.executeDartEntrypoint(
+                    DartExecutor.DartEntrypoint.createDefault()
+                )
+                FlutterEngineCache.getInstance().put(Constants.FLUTTER_ENGINE_ID, flutterEngine)
+                val flutterFragment = FlutterFragment
+                    .withCachedEngine(Constants.FLUTTER_ENGINE_ID)
+                    .renderMode(RenderMode.texture)
+                    .transparencyMode(TransparencyMode.transparent)
+                    .shouldAttachEngineToActivity(false)
+                    .build<FlutterFragment>()
+
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(fragmentContainer, flutterFragment, TAG_FLUTTER_FRAGMENT)
+                    .commit()
+            }*/
     }
 
-    fun hide(imgView: AppCompatImageView){
-        //
-    }
+    fun init(panelData: JSONObject, panelSetting: PanelSettingDataModel) {
+        this.supportFragmentManager = panelSetting.supportManager as FragmentManager?
+        this.tapppViewContainer = panelSetting.containerView as Int
 
-    fun init(mViewEnum: String, supportFragmentManager: FragmentManager, fragmentContainer: Int) {
-
-        if (mViewEnum.equals(Constants.IMAGE_VIEW)){
-            supportFragmentManager
-                .beginTransaction()
-                .add(fragmentContainer, ImageFragment(), TAG_FLUTTER_FRAGMENT)
-                .commit()
-        }else if (mViewEnum.equals(Constants.LOCAL_WEB_VIEW)){
-            val webFragment = WebFragment.newInstance(1)
-            supportFragmentManager
-                .beginTransaction()
-                .add(fragmentContainer, webFragment, TAG_FLUTTER_FRAGMENT)
-                .commit()
-        }else if (mViewEnum.equals(Constants.S3_WEB_VIEW)){
-            val webFragment = WebFragment.newInstance(2)
-            supportFragmentManager
-                .beginTransaction()
-                .add(fragmentContainer, webFragment, TAG_FLUTTER_FRAGMENT)
-                .commit()
-        }else if (mViewEnum.equals(Constants.LIBRARY_CALENDER)){
-            val webFragment = WebFragment.newInstance(3)
-            supportFragmentManager
-                .beginTransaction()
-                .add(fragmentContainer, webFragment, TAG_FLUTTER_FRAGMENT)
-                .commit()
-        }else if (mViewEnum.equals(Constants.LIBRARY_PANEL)){
-            val webFragment = WebFragment.newInstance(4)
-            supportFragmentManager
-                .beginTransaction()
-                .add(fragmentContainer, webFragment, TAG_FLUTTER_FRAGMENT)
-                .commit()
+        this.panelData = panelData
+        if (panelData.length() > 0) {
+            val mViewEnum = panelData.get("DISPLAY_OPTION")
+            if (mViewEnum.equals(Constants.LOCAL_WEB_VIEW)) {
+                mFragment = WebFragment.newInstance(1)
+            } else if (mViewEnum.equals(Constants.S3_WEB_VIEW)) {
+                mFragment = WebFragment.newInstance(2)
+            } else if (mViewEnum.equals(Constants.LIBRARY_CALENDER)) {
+                mFragment = WebFragment.newInstance(3)
+            } else if (mViewEnum.equals(Constants.LIBRARY_PANEL)) {
+                mFragment = WebFragment.newInstance(5)
+            }
+        } else {
+            mFragment = WebFragment.newInstance(5)
         }
 
-
-        /*this.supportFragmentManager = supportFragmentManager
-            this.fragmentContainer = fragmentContainer;
-            flutterFragment = supportFragmentManager
-                .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
-            if (flutterFragment == null) {
-                flutterEngine = FlutterEngine(context)
-                flutterEngine!!.dartExecutor.executeDartEntrypoint(
-                    DartExecutor.DartEntrypoint.createDefault()
-                )
-                FlutterEngineCache.getInstance().put(Constants.FLUTTER_ENGINE_ID, flutterEngine)
-                val flutterFragment = FlutterFragment
-                    .withCachedEngine(Constants.FLUTTER_ENGINE_ID)
-                    .renderMode(RenderMode.texture)
-                    .transparencyMode(TransparencyMode.transparent)
-                    .shouldAttachEngineToActivity(false)
-                    .build<FlutterFragment>()
-
-                supportFragmentManager
-                    .beginTransaction()
-                    .add(fragmentContainer, flutterFragment, TAG_FLUTTER_FRAGMENT)
-                    .commit()
-            }*/
-    }
-
-    fun init(supportFragmentManager: FragmentManager, fragmentContainer: Int) {
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(fragmentContainer, ImageFragment(), TAG_FLUTTER_FRAGMENT)
-            .commit()
-
-        /*this.supportFragmentManager = supportFragmentManager
-            this.fragmentContainer = fragmentContainer;
-            flutterFragment = supportFragmentManager
-                .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
-            if (flutterFragment == null) {
-                flutterEngine = FlutterEngine(context)
-                flutterEngine!!.dartExecutor.executeDartEntrypoint(
-                    DartExecutor.DartEntrypoint.createDefault()
-                )
-                FlutterEngineCache.getInstance().put(Constants.FLUTTER_ENGINE_ID, flutterEngine)
-                val flutterFragment = FlutterFragment
-                    .withCachedEngine(Constants.FLUTTER_ENGINE_ID)
-                    .renderMode(RenderMode.texture)
-                    .transparencyMode(TransparencyMode.transparent)
-                    .shouldAttachEngineToActivity(false)
-                    .build<FlutterFragment>()
-
-                supportFragmentManager
-                    .beginTransaction()
-                    .add(fragmentContainer, flutterFragment, TAG_FLUTTER_FRAGMENT)
-                    .commit()
-            }*/
     }
 
     fun start() {
-        /*if (supportFragmentManager != null) {
-            supportFragmentManager
-                ?.beginTransaction()
-                ?.add(fragmentContainer!!, flutterFragment!!, TAG_FLUTTER_FRAGMENT)
-                ?.commit()
-
-            //eventHandler()
-        }*/
+        if (supportFragmentManager != null) {
+            supportFragmentManager!!
+                .beginTransaction()
+                .add(tapppViewContainer, mFragment, TAG_FLUTTER_FRAGMENT)
+                .commit()
+        }
     }
 
     fun stop() {
-        /*if (supportFragmentManager != null) {
-            supportFragmentManager = null
-            fragmentContainer = null
-            removeView(fragmentContainer as Nothing?)
-        }*/
+        if (supportFragmentManager != null) {
+            supportFragmentManager!!.beginTransaction().remove(mFragment).commit()
+        }
+    }
+
+    fun subscribe(eventData: JSONObject, mPanelDataListener: DataListener?) {
+        this.mPanelDataListener = mPanelDataListener
+    }
+
+    fun unsubscribe(mPanelDataListener: DataListener?) {
+        this.mPanelDataListener = mPanelDataListener
     }
 
     private fun eventHandler() {
@@ -174,8 +147,4 @@ class OverlayPanelLayout : FrameLayout {
     }
 
     private var mPanelDataListener: DataListener? = null
-
-    fun subscribe(mPanelDataListener: DataListener?) {
-        this.mPanelDataListener = mPanelDataListener
-    }
 }
